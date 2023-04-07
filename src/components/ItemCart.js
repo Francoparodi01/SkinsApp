d8e8c608cd1a049402eda27d20a16a5ab8aabcef
Franco Parodi <francoparodi2001@gmail.com>
2023-04-07 13:40:39 -0300

    Agregando checkout

--- src/components/ItemCart.js
+++ src/components/ItemCart.js
@@ -5,14 +5,14 @@ import { useCartContext } from '../context/CartContext';
 const ItemCart = ({product}) => {
     const {removeProduct} = useCartContext();
   return (
-    <div>
+    <div className='item-cart'>
         <img  src={product.img} alt={product.name}/>
-        <div className='divCarrito'>
-            <p className='tituloCarrito'>Titulo: {product.name}</p>
-            <p className='cantidadCarrito'>Cantidad: {product.quantity}</p>
-            <p className='precioUnidadCarrito'>Precio unidad: {product.price}</p>
-            <p className='subtotalCarrito'>Subtotal: ${product.quantity * product.price}</p>
-            <button className='eliminarCarrito' onClick={()=>removeProduct(product.id)}>Eliminar</button>
+        <div className='item-details'>
+            <p className='cart-item-name'>Titulo: {product.name}</p>
+            <p className='cart-item-details'>Cantidad: {product.quantity}</p>
+            <p className='cart-item-details'>Precio unidad: {product.price}</p>
+            <p className='cart-item-details'>Subtotal: ${product.quantity * product.price}</p>
+            <button className='remove-btn' onClick={()=>removeProduct(product.id)}>Eliminar</button>
         </div>
     </div>
   )
